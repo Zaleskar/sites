9f4ab99aa2d807f67a75364d1577cade508526dd
Zaleskar <106952045+Zaleskar@users.noreply.github.com>
2023-02-08 19:39:02 +0600

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("mousemove", parallax);
-    function parallax(e) {
-        this.querySelectorAll('.image').forEach(image => {
-            const speed = image.getAttribute('data-speed');
-            const x = (window.innerWidth - e.pageX * speed) / 500;
-            const y = (window.innerHeight - e.pageY * speed) / 300;
-
-            image.style.transform = `translateX(${x}px) translateY(${y}px)`;            
-        })}
