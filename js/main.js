f1ee5d4aa0afe4106fac01e2114dc122664075ed
Zaleskar <106952045+Zaleskar@users.noreply.github.com>
2023-02-11 16:54:26 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,18 +14,40 @@ function changeColor() {
 
     let hex_code = '';
 
+
     for(let i = 0; i < 6; i++) {
         let random_index = Math.floor(Math.random() * hex_numbers.length);
 
         hex_code += hex_numbers[random_index];
-        console.log(hex_code)
+        console.log(Math.random())
+        console.log(hex_numbers.length)
+        console.log(random_index)
+        console.log(hex_numbers[random_index])
+        console.log(hex_code) 
+              
     }  
 
     document.querySelector('.hexcode').innerHTML = hex_code;
-    document.querySelector('.hexcolor').style.background = '#' + hex_code;
+    document.querySelector('.hexcolor').style.background = '#' + hex_code;    
+}
+
+function generateQuote() {
+    let quotes = {'- Oscar Wilde' : '"Be yourself; everyone else is already taken"',
+                  ' ― Mae West' : '“You only live once, but if you do it right, once is enough.”'
+};
+
+let author = Object.keys(quotes);
+let random_quotes = author[Math.floor(Math.random() * author.length)];
+let quote = quotes[random_quotes];
+console.log(quote)
+
+document.querySelector('.quotes-name').innerHTML = random_quotes;
+document.querySelector('.actual-quotes').innerHTML = quote;
 
 
-    
+
+
+
 }
 
 
